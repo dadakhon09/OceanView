6103224cf95f1fc94d25735a8d74562cc03badeb
dadakhon09 <odadaxon27@mail.ru>
2019-10-24 12:33:31 +0500

    add

--- app/models.py
+++ app/models.py
@@ -2,5 +2,7 @@ from django.db import models
 
 # Create your models here.
 
+
 class Tour(models.Model):
-    pass
+    title = models.CharField(max_length=255, null=True, blank=True)
+    description = models.TextField(null=True, blank=True)
