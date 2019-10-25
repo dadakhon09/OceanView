30889b1c798470a601c9bb6d212cdd59fd48c5e8
dadakhon09 <odadaxon27@mail.ru>
2019-10-25 21:46:31 +0500

    s

--- app/models.py
+++ app/models.py
@@ -3,6 +3,33 @@ from django.db import models
 # Create your models here.
 
 
+class Category(models.Model):
+	name = models.CharField(max_length=255, null=True, blank=True)
+
+
+class Facility(models.Model):
+	name = models.CharField(max_length=255, null=True, blank=True)
+	image = models.ImageField(upload_to='facilities', null=True, blank=True)
+
+class Expense(models.Model):
+	name = models.CharField(max_length=255, null=True, blank=True)
+	image = models.ImageField(upload_to='expenses', null=True, blank=True)
+
+
 class Tour(models.Model):
     title = models.CharField(max_length=255, null=True, blank=True)
     description = models.TextField(null=True, blank=True)
+    image = models.ImageField(upload_to='tours', null=True, blank=True)
+    route = models.CharField(max_length=255, null=True, blank=True)
+    duration = models.PositiveIntegerField(null=True, blank=True)
+    max_people = models.PositiveIntegerField(null=True, blank=True)
+    guide = models.BooleanField(null=True, blank=True)
+    price = models.PositiveIntegerField(null=True, blank=True)
+    plan = models.TextField(null=True, blank=True)
+
+
+class Sight(models.Model):
+	title = models.CharField(max_length=255, null=True, blank=True)
+	description = models.TextField(null=True, blank=True)
+	image = models.ImageField(upload_to='sights', null=True, blank=True)
+	category = models.ForeignKey(Category, on_delete=models.CASCADE)
