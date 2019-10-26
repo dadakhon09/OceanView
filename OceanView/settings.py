b1f6bbb93ac86306ceb2e8d92dab391745326171
dadakhon09 <odadaxon27@mail.ru>
2019-10-26 10:58:31 +0500

    addd

--- OceanView/settings.py
+++ OceanView/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = '7!b0$(m^wqlrjxxr$u_(kuw+5n_q@8(3)kn1rw^!-^!=5ugrz*'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['*']
 
@@ -135,10 +135,12 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.2/howto/static-files/
 
 STATIC_URL = '/static/'
+
 STATICFILES_DIRS = (
     os.path.join(BASE_DIR, 'static'),
 )
 
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
--- OceanView/urls.py
+++ OceanView/urls.py
@@ -16,6 +16,7 @@ Including another URLconf
 from django.conf import settings
 from django.conf.urls.static import static
 from django.contrib import admin
+from django.conf import settings
 from django.urls import path, include
 from django.conf.urls.i18n import i18n_patterns
 from app.others.views import handler404 as h404
@@ -34,3 +35,7 @@ urlpatterns += i18n_patterns(
     path('', include('app.urls')),
 
 ) + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+
+
+if settings.DEBUG:
+    urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
