be98282209aacc4c03d7843ef7f94243f16a92ef
dadakhon09 <odadaxon27@mail.ru>
2019-11-14 15:11:28 +0500

    addd

--- static/adminka/js/new.js
+++ static/adminka/js/new.js
@@ -1,4 +1,3 @@
-
 let Path = location.pathname.split('/').slice(1, 4);
 Path = Path.join('/');
 $('#sidebarnav li a').map(function () {
@@ -9,20 +8,80 @@ $('#sidebarnav li a').map(function () {
     }
 }).get();
 
-document.getElementById("title_en").focus();
 
 
 // Get the modal
 var modal = document.getElementById("myModal");
 
-// Get the button that opens the modal
-var btn = document.getElementById("cancelBtn");
-
 // Get the <span> element that closes the modal
 var span = document.getElementsByClassName("close")[0];
 
 var no = document.getElementsByClassName("No")[0];
+
+
+// confirm deletion
+const deleteButtons = document.querySelectorAll(".removeButton");
+for (let i = 0; i < deleteButtons.length; i++) {
+    deleteButtons[i].addEventListener("click", function (evt) {
+        evt.preventDefault();
+        const id = deleteButtons[i].dataset.id;
+        showDeletePopup(id);
+    });
+}
+
+/**
+ * @param id is id of tour that you wanna delete
+ */
+
+
+function showDeletePopup(id) {
+    function removeCharacter(str_to_remove, str) {
+        let reg = new RegExp(str_to_remove);
+        return str.replace(reg, '')
+    }
+
+    let deleteURL = document.URL + "delete/";
+    deleteURL = removeCharacter("#", deleteURL);
+
+    const deletePopupWindow = document.querySelector("#myModal");
+    const modalDeleteLink = document.querySelector("#modalDeleteButton");
+
+    deletePopupWindow.style.display = "block";
+    modalDeleteLink.setAttribute("href", deleteURL + id + "/");
+}
+
+
+
+
+// When the user clicks on <span> (x), close the modal
+span.onclick = function () {
+    modal.style.display = "none";
+};
+
+no.onclick = function () {
+    modal.style.display = "none";
+};
+// When the user clicks anywhere outside of the modal, close it
+window.onclick = function (event) {
+    if (event.target == modal) {
+        modal.style.display = "none";
+    }
+};
+
+
+
+
+
+// confirm cancelation
+
+
+// Get the button that opens the modal
+var btn = document.getElementById("cancelBtn");
+
+
+
 // When the user clicks the button, open the modal
+
 btn.onclick = function () {
     modal.style.display = "block";
 };
@@ -41,4 +100,5 @@ window.onclick = function (event) {
         modal.style.display = "none";
     }
 };
+
 
